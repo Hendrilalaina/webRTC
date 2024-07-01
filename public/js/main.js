99ded409d19dd096f829f08f00f8de11d79eb5a7
hendrilalaina <hendrilalaina@gmail.com>
2024-07-01 22:33:13 +0300

    Personal Code

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,5 @@
 import * as store from './store.js'
+import * as wss from './wss.js'
 
 const socket = io("/")
-
-socket.on('connect', () => {
-  console.log("Successfully connected to socket.io server");
-  store.setSocketId(socket.id)
-})
+wss.registerSocketEvents(socket)
