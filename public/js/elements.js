0b6e873c19e5995128939079317ab2fecb22de8a
Hendrilalaina <hendrilalaina@gmail.com>
2024-07-28 21:11:14 +0300

    Dialog

--- public/js/elements.js
+++ public/js/elements.js
@@ -4,4 +4,48 @@ export const getIncomingCallDialog = (
   rejectCallHandler
 ) => {
   console.log("getting incoming call dialog")
+  const dialog = document.createElement('div')
+  dialog.classList.add('dialog_wrapper')
+  const dialogContent = document.createElement('div')
+  dialogContent.classList.add('dialog_content')
+  dialog.appendChild(dialogContent)
+
+  const title = document.createElement('p')
+  title.classList.add('dialog_title')
+  title.innerHTML = `Incoming ${callTypeInfo} call`
+
+  const imageContainer = document.createElement('div')
+  imageContainer.classList.add('dialog_image_container')
+  const image = document.createElement('img')
+  const avatar = './utils/images/dialogAvatar.png'
+  image.src = avatar
+  imageContainer.appendChild(image)
+
+  const buttonContainer = document.createElement('div')
+  buttonContainer.classList.add('dialog_button_container')
+
+  const acceptCallButton = document.createElement('button')
+  acceptCallButton.classList.add('dialog_accept_call_button')
+  const acceptCallImg = document.createElement('img')
+  acceptCallImg.classList.add('dialog_button_container')
+  const acceptCallImgPath = './utils/images/acceptCall.png'
+  acceptCallImg.src = acceptCallImgPath
+  acceptCallButton.appendChild(acceptCallImg)
+  buttonContainer.appendChild(acceptCallButton)
+
+  const rejectCallButton = document.createElement('button')
+  rejectCallButton.classList.add('dialog_reject_call_button')
+  const rejectCallImg = document.createElement('img')
+  rejectCallImg.classList.add('dialog_button_container')
+  const rejectCallImgPath = './utils/images/rejectCall.png'
+  rejectCallImg.src = rejectCallImgPath
+  rejectCallButton.appendChild(rejectCallImg)
+  buttonContainer.appendChild(rejectCallButton)
+
+  dialogContent.appendChild(title)
+  dialogContent.appendChild(imageContainer)
+  dialogContent.appendChild(buttonContainer)
+
+  const dialogHTML = document.getElementById('dialog')
+  dialogHTML.appendChild(dialog)
 }
--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ import * as store from './store.js'
 import * as wss from './wss.js'
 import * as webRTCHandler from './webRTCHandler.js'
 import * as constants from './constants.js'
+import { getIncomingCallDialog } from './elements.js'
 
 const socket = io("/")
 wss.registerSocketEvents(socket)
@@ -34,3 +35,4 @@ personalCodeVideoButton.addEventListener('click', () => {
   webRTCHandler.sendPreOffer(callType, calleePersonalCode)
 })
 
+getIncomingCallDialog('VIDEO', () => {}, () => {})
